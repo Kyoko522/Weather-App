f63729799daa58521a09d3f967c7ec0aae6ec06d
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-08-18 22:46:02 -0400

    formatted

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -4,37 +4,37 @@ function TopButtons() {
 
     const cities = [
         {
-        id:1,
-        title: 'London'
-    },
+            id: 1,
+            title: 'London'
+        },
 
-    {
-        id:2,
-        title: 'Sydney'
-    },
+        {
+            id: 2,
+            title: 'Sydney'
+        },
 
-    {
-        id:3,
-        title: 'Tokyo'
-    },
-    {
-        id:4,
-        title: 'Toronto'
-    },
-    {
-        id:5,
-        title: 'Paris'
-    },
+        {
+            id: 3,
+            title: 'Tokyo'
+        },
+        {
+            id: 4,
+            title: 'Toronto'
+        },
+        {
+            id: 5,
+            title: 'Paris'
+        },
 
-]
+    ]
 
 
-  return (
-    <div className="flex items-center justify-around my-6">
-        {cities.map((city) => (
-            <button key={cities.id} className="text-green-500 text-lg font-medium">{city.title}</button>
-        ))}
-    </div>
+    return (
+        <div className="flex items-center justify-around my-6">
+            {cities.map((city) => (
+                <button key={cities.id} className="text-green-500 text-lg font-medium">{city.title}</button>
+            ))}
+        </div>
     );
 }
 export default TopButtons;
